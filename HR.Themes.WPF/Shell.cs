75413f9559600f8fd664cc5efcb6d74d03bd7c13
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-15 20:17:37 +0530

    Add project files.

---
+++ HR.Themes.WPF/Shell.cs
@@ -0,0 +1,38 @@
+﻿using HR.Themes.Core;
+using HR.Themes.Prototype.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HR.Themes.WPF
+{
+    public class Shell : IShell
+    {
+        public bool IsLightVersion { get { return false; } }
+
+        public void ExitApplication()
+        {
+            App.Current.Shutdown();
+        }
+
+        public void ShowMessage(string message, string title)
+        {
+            MessageBox.Show(message, title);
+        }
+
+        public IWindow CreateWindow(object userControl, string title)
+        {
+            var window = new WindowControl();
+            var child = (UserControl)userControl;
+            window.Content = child;
+            window.Title = title;
+            window.SizeToContent = SizeToContent.WidthAndHeight;
+            window.Show();
+
+            return window;
+        }
+    }
+}
---
+++ HR.Themes.WPF/WindowControl.cs
@@ -0,0 +1,18 @@
+﻿using HR.Themes.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace HR.Themes.WPF
+{
+    public class WindowControl: Window, IWindow
+    {
+        void IWindow.Show()
+        {
+            base.Show();
+            base.Activate();
+        }
+    }
+}
